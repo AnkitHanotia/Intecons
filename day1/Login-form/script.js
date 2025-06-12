00475b45b6ad2878c470aa73157c2bc2a2d117f2
Ankit Hanotia <ankit04hanotia9771@gmail.com>
2025-06-12 11:43:00 +0530

    day1 done

---
+++ day1/Login-form/script.js
@@ -0,0 +1,8 @@
+const userEmail = document.querySelector(".userEmail input");
+const userPass = document.querySelector(".userPass input");
+const btn = document.querySelector(".sub-btn");
+btn.addEventListener('submit', (e)=>{
+    userEmail.addEventListener("input", (inp)=>{
+        const mail = inp.input;
+    })
+});
